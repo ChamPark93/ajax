50b4fda8c539eb022e377feecb7cb40975a3bf74
ChamPark93 <smreotls0405@naver.com>
2022-09-27 14:19:14 +0900

    d

--- sendRequest-ajax.js
+++ sendRequest-ajax.js
@@ -1,8 +1,7 @@
 /************************************************************************************
 
-sendRequest-ajax
+sendRequest-ajax Branches 버전
 
-author : 허정진  http://tistory.serpiko.com
 author : 허정진  http://serpiko.tistory.com
 
 update : 2017. 11. 08
